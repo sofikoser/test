059b41cff0d430b40cccf5f54bd5cce36897e786
sofikoser <ssssofisss@gmail.com>
2026-01-02 19:20:46 +0300

    2

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
-const FirstName='Sofia'
-console.log('Hello ', FirstName)
+const firstName = "Sofiya";
+let age = 26;
+let city = "Aktau";
+let hobby = "handmade crafts";
+
+console.log(
+  `My name is ${firstName}, I am ${age} years old, I live in ${city}, and my hobby is ${hobby}.`
+);
